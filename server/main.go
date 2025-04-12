25379a11cad5b198e24d8a48049ab25237159d26
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-04-13 01:49:10 +0530

    feat: Added logger middleware

--- server/main.go
+++ server/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
+	"github.com/rocvitc/hackvault/server/api"
 	"github.com/rocvitc/hackvault/server/config"
+	"github.com/rocvitc/hackvault/server/utils"
+	"github.com/rocvitc/hackvault/server/utils/middleware"
 )
 
 func main() {
@@ -14,5 +18,16 @@ func main() {
 	} else {
 		cfg = config
 	}
-	fmt.Println(cfg)
+	utils.NewLogger(cfg.Prod)
+	if cfg.Prod {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		gin.SetMode(gin.DebugMode)
+	}
+	r := gin.New()
+	r.Use(middleware.Logger())
+	r.Use(gin.Recovery())
+	api.LoadRoutes(r)
+	fmt.Printf("[ENGINE] Server started at %s:%d\n", cfg.Host, cfg.Port)
+	r.Run(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port))
 }
