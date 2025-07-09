a87c4cc071e8b865b7f65edcad21e5463be6a5b2
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-07-09 14:15:51 +0530

    ref: Rename dirs

--- api/api.go
+++ api/api.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/intraware/rodan/api/challenges"
+	"github.com/intraware/rodan/api/team"
+	"github.com/intraware/rodan/api/user"
 )
 
 func LoadRoutes(r *gin.Engine) {
@@ -9,9 +12,9 @@ func LoadRoutes(r *gin.Engine) {
 	// LoadAuth(apiRouter)
 	LoadLeaderBoard(apiRouter)
 	LoadNotification(apiRouter)
-	//challenges.LoadChallenges(apiRouter)
-	//team.LoadTeam(apiRouter)
-	//user.LoadUser(apiRouter)
+	challenges.LoadChallenges(apiRouter)
+	team.LoadTeam(apiRouter)
+	user.LoadUser(apiRouter)
 
 	apiRouter.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"msg": "pong"})
