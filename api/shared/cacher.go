acaa02c78ede68dd491ea0de783ceb89d9d2fe6f
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2025-09-30 01:58:45 +0530

    fix: Changed the initialization of cacheObj to Init()

--- api/shared/cacher.go
+++ api/shared/cacher.go
@@ -1,46 +1,13 @@
 package shared
 
 import (
-	"time"
-
 	"github.com/intraware/rodan/internal/cache"
 	"github.com/intraware/rodan/internal/models"
 )
 
-var UserCache = cache.NewCache[uint, models.User](&cache.CacheOpts{
-	TimeToLive:    3 * time.Minute,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(true),
-})
-
-var TeamCache = cache.NewCache[uint, models.Team](&cache.CacheOpts{
-	TimeToLive:    3 * time.Minute,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(true),
-})
-
-var ChallengeCache = cache.NewCache[uint, models.Challenge](&cache.CacheOpts{
-	TimeToLive:    3 * time.Minute,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(true),
-})
-
-var TeamSolvedCache = cache.NewCache[string, bool](&cache.CacheOpts{
-	TimeToLive:    0,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(false),
-})
-
-var StaticConfig = cache.NewCache[uint, models.StaticConfig](&cache.CacheOpts{
-	TimeToLive:    3 * time.Minute,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(true),
-})
-
-var BanHistoryCache = cache.NewCache[string, models.BanHistory](&cache.CacheOpts{
-	TimeToLive:    10 * time.Minute,
-	CleanInterval: ptr(time.Hour * 2),
-	Revaluate:     ptr(true),
-})
-
-func ptr[T any](v T) *T { return &v }
+var UserCache cache.Cache[uint, models.User]
+var TeamCache cache.Cache[uint, models.Team]
+var ChallengeCache cache.Cache[uint, models.Challenge]
+var TeamSolvedCache cache.Cache[string, bool]
+var StaticConfig cache.Cache[uint, models.StaticConfig]
+var BanHistoryCache cache.Cache[string, models.BanHistory]
--- api/shared/init.go
+++ api/shared/init.go
@@ -1,10 +1,47 @@
 package shared
 
 import (
+	"time"
+
+	"github.com/intraware/rodan/internal/cache"
 	"github.com/intraware/rodan/internal/config"
+	"github.com/intraware/rodan/internal/models"
 )
+
+func ptr[T any](v T) *T { return &v }
 
 func Init(config *config.Config) {
 	UserBlackList = config.App.Leaderboard.UserBlackList
 	TeamBlackList = config.App.Leaderboard.TeamBlackList
+
+	UserCache = cache.NewCache[uint, models.User](&cache.CacheOpts{
+		TimeToLive:    3 * time.Minute,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(true),
+	})
+	TeamCache = cache.NewCache[uint, models.Team](&cache.CacheOpts{
+		TimeToLive:    3 * time.Minute,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(true),
+	})
+	ChallengeCache = cache.NewCache[uint, models.Challenge](&cache.CacheOpts{
+		TimeToLive:    3 * time.Minute,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(true),
+	})
+	TeamSolvedCache = cache.NewCache[string, bool](&cache.CacheOpts{
+		TimeToLive:    0,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(false),
+	})
+	StaticConfig = cache.NewCache[uint, models.StaticConfig](&cache.CacheOpts{
+		TimeToLive:    3 * time.Minute,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(true),
+	})
+	BanHistoryCache = cache.NewCache[string, models.BanHistory](&cache.CacheOpts{
+		TimeToLive:    10 * time.Minute,
+		CleanInterval: ptr(time.Hour * 2),
+		Revaluate:     ptr(true),
+	})
 }
